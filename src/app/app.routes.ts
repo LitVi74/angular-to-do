ee539c2b4678c9058759b0a12b1eb52aa65da8f4
Anastasiya <progr20@pancir.it>
2024-08-07 20:03:24 +0500

    init home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    loadComponent: () => import('./pages/home-page/home-page.component').then(({HomePageComponent}) => HomePageComponent)
+  }
+];
