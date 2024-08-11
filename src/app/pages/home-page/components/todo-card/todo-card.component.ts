2ddcf80b1ed8112143eea0b7ce3a836ca2c07fcd
Anastasiya <progr20@pancir.it>
2024-08-11 13:34:55 +0500

    add styles to page

--- src/app/pages/home-page/components/todo-card/todo-card.component.ts
+++ src/app/pages/home-page/components/todo-card/todo-card.component.ts
@@ -1,10 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
+import { TuiButton, TuiSurface, TuiTitle } from '@taiga-ui/core';
+import { TuiCheckbox } from '@taiga-ui/kit';
+import { FormsModule } from '@angular/forms';
 import { ITodo } from '../../../../reducers/todos/todos.reducer';
 
 @Component({
   selector: 'app-todo-card',
   standalone: true,
-  imports: [],
+  imports: [TuiCardLarge, TuiTitle, TuiHeader, TuiSurface, TuiButton, TuiCheckbox, FormsModule],
   templateUrl: './todo-card.component.html',
   styleUrl: './todo-card.component.scss',
 })
